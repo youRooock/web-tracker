a3360bcb53c9de841676ba18696a2fd1d108706b
youRooock <junioriks@rambler.ru>
2019-09-09 21:24:43 +0300

    updated background logic

--- web-tracker-extension-fe/public/background.js
+++ web-tracker-extension-fe/public/background.js
@@ -1,35 +1,47 @@
 import authenticatedXhr from "./authenticatedXhr.js";
 
-chrome.webNavigation.onCompleted.addListener((details) => {
+chrome.webNavigation.onCompleted.addListener(details => {
   if (details.frameId == 0) {
-    const url = new URL(details.url);
- 
     // handle cases with empty new tab
-    chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true },
-      (tabs) => {
-        console.log("activated " + tabs[0].url);
-      });
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+      console.log("first navigation to " + getHostName(details.url));
+    });
 
-    authenticatedXhr('POST', 'http://localhost:5000', 'application/x-www-form-urlencoded', 'url=' + url.hostname);
+    authenticatedXhr({
+      method: "POST",
+      url: "http://localhost:5000",
+      type: "application/x-www-form-urlencoded",
+      body: "url=" + getHostName(details.url)
+    });
   }
 });
 
-chrome.tabs.onActivated.addListener((details) => {
-  chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true },
-    (tabs) => {
-      if (tabs[0].url) console.log("activated " + tabs[0].url);
-    });
+chrome.tabs.onActivated.addListener(() => {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    if (tabs[0].url) {
+      console.log("activated " + getHostName(tabs[0].url));
+    }
+  });
 });
 
 // handle last tab close with multiple windows
-chrome.tabs.onRemoved.addListener((details) => {
-
-  chrome.tabs.query({ 'active': true, 'lastFocusedWindow': false },
-    (tabs) => {
-      if (tabs.length != 0) console.log('activated ' + tabs[0].url);
-    });
-
+// switch to last window active tab
+chrome.tabs.onRemoved.addListener(() => {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    if (tabs.length == 0) {
+      chrome.tabs.query({ active: true, lastFocusedWindow: false }, tabs => {
+        if (tabs.length != 0 && tabs[tabs.length - 1].url) {
+          console.log("activated " + getHostName(tabs[0].url));
+        }
+      });
+    }
+  });
 });
 // chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
 //   sendResponse({ email: email });
 // });
+
+
+const getHostName = (url) => {
+ return new URL(url).hostname;
+}
